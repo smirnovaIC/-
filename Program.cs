808a2d2ec144366a8cd86da186a5c784e33cd523
smirnovaIC <115992227+smirnovaIC@users.noreply.github.com>
2022-10-17 15:02:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Критерии_оценивания
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            string res = a >= 90 && a <= 100 ? "Отлично" : a >= 70 ? "Хорошо" :
+                a >= 50 ? "Удовлетворительно" : "Неудовлетворительно";
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
